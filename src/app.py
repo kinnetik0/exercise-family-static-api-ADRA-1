904738d799fff900c388e628e9d2f09add53938e
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-07-14 14:08:50 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
-from src.datastructure import FamilyStructure  # Asegúrate de que la ruta sea correcta
+from datastructure import FamilyStructure  # Asegúrate de que la ruta sea correcta
+from utils import APIException, generate_sitemap
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,6 +10,31 @@ CORS(app)
 
 # Crea el objeto de la familia Jackson
 jackson_family = FamilyStructure("Jackson")
+
+John = {
+    "first_name": "John",
+    "last_name": jackson_family.last_name,
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "last_name": jackson_family.last_name,
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "last_name": jackson_family.last_name,
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
 
 # Maneja/serializa errores como un objeto JSON
 @app.errorhandler(APIException)
@@ -55,14 +81,14 @@ def delete_member(member_id):
         return jsonify({"message": "Member not found"}), 404
 
 # Actualizar un miembro por ID
-@app.route('/member/<int:member_id>', methods=['PATCH'])
-def update_member(member_id):
-    data = request.get_json()
-    result = jackson_family.update_member(member_id, data)
-    if result["done"]:
-        return jsonify(result), 200
-    else:
-        return jsonify({"message": "Member not found"}), 404
+# @app.route('/member/<int:member_id>', methods=['PATCH'])
+# def update_member(member_id):
+#     data = request.get_json()
+#     result = jackson_family.update_member(member_id, data)
+#     if result["done"]:
+#         return jsonify(result), 200
+#     else:
+#         return jsonify({"message": "Member not found"}), 404
 
 # Esto solo se ejecuta si se ejecuta $ python src/app.py
 if __name__ == '__main__':
--- src/datastructure.py
+++ src/datastructure.py
@@ -4,20 +4,13 @@ class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = [
-            {"id": self._generate_id(), "first_name": "John", "last_name": last_name, "age": 33, "lucky_numbers": [7, 13, 22]},
-            {"id": self._generate_id(), "first_name": "Jane", "last_name": last_name, "age": 35, "lucky_numbers": [10, 14, 3]},
-            {"id": self._generate_id(), "first_name": "Jimmy", "last_name": last_name, "age": 5, "lucky_numbers": [1]}
-        ]
+        self._members = []
 
-    # Generate a unique ID for each member
-    def _generate_id(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
+    def _generateId(self):
+        return randint(0, 99999999)
 
     def add_member(self, member):
-        member["id"] = self._generate_id()
+        member["id"] = self._generateId()
         member["last_name"] = self.last_name
         self._members.append(member)
         return member
@@ -35,18 +28,18 @@ class FamilyStructure:
                 return member
         return None
 
-    def update_member(self, id, member):
-        for i, existing_member in enumerate(self._members):
-            if existing_member["id"] == id:
-                self._members[i] = {
-                    "id": id,
-                    "last_name": self.last_name,
-                    "first_name": member["first_name"],
-                    "age": member["age"],
-                    "lucky_numbers": member["lucky_numbers"]
-                }
-                return {"done": True}
-        return {"done": False}
+    # def update_member(self, id, member):
+    #     for i, existing_member in enumerate(self._members):
+    #         if existing_member["id"] == id:
+    #             self._members[i] = {
+    #                 "id": id,
+    #                 "last_name": self.last_name,
+    #                 "first_name": member["first_name"],
+    #                 "age": member["age"],
+    #                 "lucky_numbers": member["lucky_numbers"]
+    #             }
+    #             return {"done": True}
+    #     return {"done": False}
 
     def get_all_members(self):
         return self._members
